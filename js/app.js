1a4c2d1a874733164c2bc62731567fdb6f3854dd
3fun4 <zz3fun4@gmail.com>
2018-01-02 21:28:35 +0800

    check style

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /*
  * Create a list that holds all of your cards
  */
-
 var cardList = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-anchor','fa-leaf','fa-bicycle','fa-diamond','fa-bomb','fa-leaf','fa-bomb','fa-bolt','fa-bicycle','fa-paper-plane-o','fa-cube'];
 
 // numbers of moves
@@ -33,19 +32,8 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 //reset timer
-function resetTimer(){
+function resetTimer() {
 	timer.stop();
 	$('#timer').html('00:00:00');
 }
@@ -57,38 +45,38 @@ function initScorePanel() {
 }
 
 //update moves
-function updateMoves(){
+function updateMoves() {
 	moves ++;
 	$('.moves').text(moves);
 	updateStars();
 }
 
 //reset stars
-function resetStars(){
+function resetStars() {
 	$('#stars li').each(function(){
 		$(this).children().removeClass('fa fa-star-o').addClass('fa fa-star');
 	});
 }
 
 //update stars
-function updateStars(){
-	if(moves>23){
+function updateStars() {
+	if (moves>23) {
 		$('#stars li:nth-child(2)').children().removeClass('fa fa-star').addClass('fa fa-star-o');
 		$('#stars li:nth-child(3)').children().removeClass('fa fa-star').addClass('fa fa-star-o');
-	} else if(moves>14 && moves<=23){
+	} else if (moves>14 && moves<=23) {
 		$('#stars li:nth-child(3)').children().removeClass('fa fa-star').addClass('fa fa-star-o');
 	}
 }
 
 // Initial the card deck
-function initGame(){
+function initGame() {
 
 	openCards = [];
 
 	var cards = shuffle(cardList);
 
 	$('.deck').empty();
-	for(var i=0;i<cards.length;i++){
+	for (var i=0; i<cards.length; i++) {
 		$('.deck').append('<li class="animated card"><i class="fa '+cards[i]+'"></i></li>');
 	}
 
@@ -99,7 +87,7 @@ function initGame(){
 }
 
 //reset the game
-function resetGame(){
+function resetGame() {
 	gameStarted = false;
 	openCards = [];
 	matchFound = 0;
@@ -119,10 +107,10 @@ function clickCard(card) {
 
 	var cardClass = $(card).find('i').attr('class');
 
-	if(openCards.length===0) {
+	if (openCards.length===0) {
 		$(card).toggleClass('open show').animateCss('flipInY');
 		openCards.push(cardClass);
-	} else if(openCards.length===1) {
+	} else if (openCards.length===1) {
 		updateMoves();
 		$(card).addClass('open show').animateCss('flipInY');
 		openCards.push(cardClass);
@@ -132,8 +120,8 @@ function clickCard(card) {
 }
 
 //check whether the opened cards are matched
-function matchOpenCards(){
-	if(openCards[0]==openCards[1]) {
+function matchOpenCards() {
+	if (openCards[0]==openCards[1]) {
 		$('.deck').find('.card.open.show').off('click');
 		$('.deck').find('.card.open.show').removeClass('open show').addClass('match').animateCss('pulse');
 		checkWin();
@@ -153,22 +141,21 @@ function checkWin() {
 }
 
 //show game result
-function showResult(){
+function showResult() {
 	swal({
 		allowOutsideClick:false,
 		type: 'success',
 		title: 'Congrats!',
 		html:
-			'<div class="success-score">'
-			+	'<section class="score-panel">'
-			+		'<ul class="stars">'
-			+ 			$('#stars').html()
-        	+		'</ul>'
-			+	'</section>'
-
-			+'</div>'
-			+'<span class="success-score">Moves:'+moves+'</span>'
-			+'<span class="success-score">Time:'+timer.getTimeValues().toString()+'</span>'
+			'<div class="success-score">'+
+				'<section class="score-panel">'+
+					'<ul class="stars">'+
+			 			$('#stars').html()+
+        			'</ul>'+
+				'</section>'+
+			'</div>'+
+			'<span class="success-score">Moves:'+moves+'</span>'+
+			'<span class="success-score">Time:'+timer.getTimeValues().toString()+'</span>'
 	});
 }
 
